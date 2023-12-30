46e288bd7114470e6345c83e7eda5a0b86d560b6
vaibhav <vaibhavmasetty06@gmail.com>
2023-12-30 15:59:31 -0500

    first commit

---
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
